aad5cf87db47dffde44da069442b5f161bf0bf0a
Lauren Langley <llangley@cse-mbp-01558.local>
2015-10-14 11:02:37 -0400

    added subnav sticky

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
+$(document).ready(function() {
 
-$(document).on("scroll", function(){
-  if
-    ($(document).scrollTop() > 100){
-    $("nav").addClass("shrink");
-  }
-  else
-  {
-    $("nav").removeClass("shrink");
-  }
-});
-
-$(".utility").on("click", function() {
-  if ($("nav").hasClass("shrink")) {
-    $("nav").removeClass("shrink");
-    if ($(".subnav").hasClass("affix")) {
-      $(".subnav").removeClass("affix")
-      $(".subnav").addClass("affix-top")
+  $(document).on("scroll", function(){
+    if
+      ($(document).scrollTop() > 100){
+      $("nav").addClass("shrink");
     }
-  }
-  window.scrollTo(0, 0);
-})
+    else
+    {
+      $("nav").removeClass("shrink");
+    }
+  });
 
-$(".subnav").affix({
-  offset: {
-    top: $(".one").height()
-  }
-});
+  $(".utility").on("click", function() {
+    if ($("nav").hasClass("shrink")) {
+      $("nav").removeClass("shrink");
+      if ($(".subnav").hasClass("affix")) {
+        $(".subnav").removeClass("affix")
+        $(".subnav").addClass("affix-top")
+      }
+    }
+    window.scrollTo(0, 0);
+  })
 
-$('.subnav').on('affixed.bs.affix', function(){
-  $(".indicator").addClass("active");
-});
+  $(".subnav").affix({
+    offset: {
+      top: $(".one").height()
+    }
+  });
 
-$('.subnav').on('affixed-top.bs.affix', function(){
-  $(".indicator").removeClass("active");
+  $('.subnav').on('affixed.bs.affix', function(){
+    $(".indicator").addClass("active");
+  });
+
+  $('.subnav').on('affixed-top.bs.affix', function(){
+    $(".indicator").removeClass("active");
+  });
+
 });
