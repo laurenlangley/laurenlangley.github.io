f32236dd5248cd636397389f52a1caab8e155120
Lauren Langley <llangley@cse-mbp-01558.local>
2015-10-14 10:59:22 -0400

    added subnav sticky

--- js/script.js
+++ js/script.js
@@ -3,11 +3,34 @@ $(document).on("scroll", function(){
   if
     ($(document).scrollTop() > 100){
     $("nav").addClass("shrink");
-    updateSliderMargin();
   }
   else
   {
     $("nav").removeClass("shrink");
-    updateSliderMargin();
   }
+});
+
+$(".utility").on("click", function() {
+  if ($("nav").hasClass("shrink")) {
+    $("nav").removeClass("shrink");
+    if ($(".subnav").hasClass("affix")) {
+      $(".subnav").removeClass("affix")
+      $(".subnav").addClass("affix-top")
+    }
+  }
+  window.scrollTo(0, 0);
+})
+
+$(".subnav").affix({
+  offset: {
+    top: $(".one").height()
+  }
+});
+
+$('.subnav').on('affixed.bs.affix', function(){
+  $(".indicator").addClass("active");
+});
+
+$('.subnav').on('affixed-top.bs.affix', function(){
+  $(".indicator").removeClass("active");
 });
