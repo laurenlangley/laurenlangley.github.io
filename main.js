bbe794a7e4aa29a850797273fdc62c67a68a0ea6
Lauren Langley <llangley@cse-mbp-01558.local>
2016-02-24 23:04:19 -0500

    starting over ajax

--- main.js
+++ main.js
@@ -1,45 +1,55 @@
 $(document).ready(function () {
 
-    function loadJSON(callback) {
-
-        var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'data.json', true);
-
-        // xobj.addEventListener('load', function() {
-        //     if(xobj.status === 200){
-        //         callback(xobj.responseText);
-        //     }
-        // },false)
-
-        xobj.onreadystatechange = function() {
-            if (xobj.readyState == 4 && xobj.status == "200") {
-                // .open will NOT return a value but simply returns undefined in async mode so use a callback
-                callback(xobj.responseText);
-            }
+    function getBuses() {
+     $.ajax({
+        type: 'GET',
+        url: 'data.json',
+        //data: { get_param: 'value' }, 
+        data: null,
+        dataType: 'json',
+        success: function(data) { 
+            $.each(data, function(index, element) {
+                $('body').append($('<div>', {
+                    text: element.VEHICLE
+                }));
+            });
         }
-        xobj.send(null);
+    });
     }
 
-    // Call to function with anonymous callback
-    loadJSON(function(response) {
-        // Do Something with the response e.g.
-        jsonresponse = JSON.parse(response);
+    getBuses()
 
-        // Assuming json data is wrapped in square brackets as Drew suggests
-        console.log(jsonresponse)
-        //console.log(jsonresponse[0].ROUTE);
+    // function loadJSON(callback) {
 
-        var section;
+    //     var xobj = new XMLHttpRequest();
+    //     xobj.overrideMimeType("application/json");
+    //     xobj.open('GET', 'data.json', true);
+    //     xobj.onreadystatechange = function() {
+    //         if (xobj.readyState == 4 && xobj.status == "200") {
+    //             callback(xobj.responseText);
+    //         }
+    //     }
+    //     xobj.send(null);
+    // }
 
-        for (var i = 0; i < jsonresponse.length; i++) {
-            section = $('<section>');
-            section.append("<div>Bus " + jsonresponse[i].VEHICLE + " is at " + jsonresponse[i].LATITUDE + " and " + jsonresponse[i].LONGITUDE + " at " + jsonresponse[i].MSGTIME + + "</div>");
-            $('body').append(section);
-        }
+    // // Call to function with anonymous callback
+    // loadJSON(function(response) {
+    //     // Do Something with the response e.g.
+    //     jsonresponse = JSON.parse(response);
+
+    //     // Assuming json data is wrapped in square brackets as Drew suggests
+    //     console.log(jsonresponse)
+    //     //console.log(jsonresponse[0].ROUTE);
+
+    //     var section;
+
+    //     for (var i = 0; i < jsonresponse.length; i++) {
+    //         section = $('<section>');
+    //         section.append("<div>Bus " + jsonresponse[i].VEHICLE + " is at " + jsonresponse[i].LATITUDE + " and " + jsonresponse[i].LONGITUDE + " at " + jsonresponse[i].MSGTIME + + "</div>");
+    //         $('body').append(section);
+    //     }
         
-    });
+    // });
 
-    setTimeout(loadJSON, 10000);
 
 });
